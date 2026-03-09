f087601109d6a2fd6a2366981cb78c25a0882b77
shoumen-shomu <shoumenshomu@gmail.com>
2026-03-09 13:19:10 +0600

    create layouts folder on component

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,24 +1,12 @@
+import Banner from "@/component/layouts/home/Banner";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="h-screen flex items-center justify-center  bg-gray-300 ">
-      <div>
-        <h1 className="text-[65px] font-bold text-white">
-          Wellcome to Home page
-        </h1>
-
-        <button className="py-7">
-          <Link
-            className="py-2 px-4 border rounded-md text-2xl font-medium"
-            to={"/shop"}
-          >
-            Go to Shop
-          </Link>
-        </button>
-      </div>
-    </div>
+<>
+<Banner/>
+</>
   );
 };
 
