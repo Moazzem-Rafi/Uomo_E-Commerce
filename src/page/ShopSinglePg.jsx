5dc5923bc988b151d1727ba142bbf3069414cd49
basudevkumar <jhulonkumar40@gmail.com>
2026-03-10 22:46:22 +0600

    fix: in ShopSinglePg.jsx fixed 'Tabs_desc' name correct for vercel deployment conflict avoid

--- src/page/ShopSinglePg.jsx
+++ src/page/ShopSinglePg.jsx
@@ -1,14 +1,15 @@
 import Add_review from "@/component/shopMain/shopSingle/Add_review";
 import Shopsingle from "@/component/shopMain/shopSingle/Shopsingle";
-import Tabs_desc from "@/component/shopMain/shopSingle/tabs_desc";
+import Tabs_desc from "@/component/shopMain/shopSingle/Tabs_desc";
+
 import React from "react";
 
 const ShopSinglePg = () => {
   return (
     <>
       <Shopsingle />
-      <Tabs_desc/>
-      <Add_review/>
+      <Tabs_desc />
+      <Add_review />
     </>
   );
 };
