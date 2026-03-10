2f2c3de4deeb0770ebd2027eaaa43d3829fc2ed5
Md Jaber Hossain <md9292463@gmail.com>
2026-03-10 21:39:01 +0600

    feat: Tabs_desc design

--- src/page/ShopSinglePg.jsx
+++ src/page/ShopSinglePg.jsx
@@ -1,14 +1,12 @@
 import Shopsingle from "@/component/shopMain/shopSingle/Shopsingle";
+import Tabs_desc from "@/component/shopMain/shopSingle/tabs_desc";
 import React from "react";
 
 const ShopSinglePg = () => {
   return (
     <>
       <Shopsingle />
-      {/**
-       * name : Jaber
-       * message : akhane apni ./component/shopSingle theke all jaja related component ache  akhane import hobe....
-       */}
+      <Tabs_desc/>
     </>
   );
 };
