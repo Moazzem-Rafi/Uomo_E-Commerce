69cb9fb9f96469ac8e310a2463396836636011ce
Md Jaber Hossain <md9292463@gmail.com>
2026-03-12 00:14:32 +0600

    all done

--- src/component/shopMain/shopSingle/Shopsingle.jsx
+++ src/component/shopMain/shopSingle/Shopsingle.jsx
@@ -9,7 +9,7 @@ import { FaRegHeart } from "react-icons/fa6";
 import { BsShare } from "react-icons/bs";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Navigation } from "swiper/modules"; 
+import { Navigation } from "swiper/modules";
 import "swiper/css/navigation";
 import { useState } from "react";
 
@@ -17,29 +17,38 @@ const Shopsingle = () => {
   let [count, setCount] = useState(3);
   const handleMinus = () => {
     if (count > 0) {
-      setCount((count -= 1));
+      setCount(count - 1);
     }
   };
   const handlePlus = () => {
     if (count < 10) {
-      setCount((count += 1));
+      setCount(count + 1);
     }
   };
+
   return (
-    // Top
-    <div className="mt-12.5 mb-25">
+    <div className="my-8  lg:mt-12.5 lg:mb-25">
       <Container>
-        <div className="flex gap-x-15">
-          {/* Flex One  */}
-          <div className="flex gap-x-2.5">
-            <div className="space-y-2.5">
-              <Images imgSrc={sssOne} />
-              <Images imgSrc={sssOne} />
-              <Images imgSrc={sssOne} />
-              <Images imgSrc={sssOne} />
+        <div className="flex flex-col lg:flex-row gap-y-8 lg:gap-x-15">
+          {/* Left Side: Images */}
+          <div className="flex flex-col-reverse lg:flex-row gap-2.5 ">
+            <div className="flex lg:flex-col gap-2.5 overflow-x-auto lg:overflow-visible ">
+              <div className="min-w-[80px] lg:w-auto">
+                <Images imgSrc={sssOne} />
+              </div>
+              <div className="min-w-[80px] lg:w-auto">
+                <Images imgSrc={sssOne} />
+              </div>
+              <div className="min-w-[80px] lg:w-auto">
+                <Images imgSrc={sssOne} />
+              </div>
+              <div className="min-w-[80px] lg:w-auto">
+                <Images imgSrc={sssOne} />
+              </div>
             </div>
-            <div className="w-170 relative group">
-              {/* Slider */}
+
+            {/* Main Slider */}
+            <div className="w-full lg:w-[570px] relative group">
               <Swiper
                 modules={[Navigation]}
                 navigation={{
@@ -48,120 +57,113 @@ const Shopsingle = () => {
                 }}
                 spaceBetween={10}
                 slidesPerView={1}
-                onSlideChange={() => console.log("slide change")}
-                onSwiper={(swiper) => console.log(swiper)}
                 className="w-full h-auto"
               >
-                <SwiperSlide className="relative flex justify-center items-center bg-white">
-                  <Images
-                    imgSrc={ssd}
-                    className="w-full h-auto object-contain"
-                  />
-                  <Images
-                    imgSrc={zoomIcon}
-                    className={
-                      "absolute  bottom-7.5 right-7.5 h-6 l"
-                    }
-                  />
-                </SwiperSlide>
-                <SwiperSlide className="relative flex justify-center items-center bg-white">
-                  <Images
-                    imgSrc={ssd}
-                    className="w-full h-auto object-contain"
-                  />
-                  <Images
-                    imgSrc={zoomIcon}
-                    className={
-                      "absolute bottom-7.5 right-7.5  h-6 "
-                    }
-                  />
-                </SwiperSlide>
-                <SwiperSlide className="relative flex justify-center items-center bg-white">
-                  <Images
-                    imgSrc={ssd}
-                    className="w-full h-auto object-contain"
-                  />
-                  <Images
-                    imgSrc={zoomIcon}
-                    className={
-                      "absolute bottom-7.5 right-7.5  h-6 "
-                    }
-                  />
-                </SwiperSlide>
+                {[1, 2, 3].map((_, index) => (
+                  <SwiperSlide
+                    key={index}
+                    className="relative flex justify-center items-center bg-[#F5F5F5]"
+                  >
+                    <Images
+                      imgSrc={ssd}
+                      className="w-full h-auto object-contain"
+                    />
+                    <Images
+                      imgSrc={zoomIcon}
+                      className="absolute bottom-4 right-4 lg:bottom-7.5 lg:right-7.5 h-6 w-6 cursor-pointer"
+                    />
+                  </SwiperSlide>
+                ))}
               </Swiper>
 
-              {/* slider next prev buttons */}
-              <div className="button-prev-custom absolute left-2 lg:left-7.5 top-1/2 -translate-y-1/2 z-10 cursor-pointer bg-white text-[#767676]  p-4 rounded-full shadow-md flex items-center justify-center">
-                <GrFormPrevious size={24} />
+              {/* Slider Arrows */}
+              <div className="button-prev-custom absolute left-2 lg:left-5 top-1/2 -translate-y-1/2 z-10 cursor-pointer bg-white/80 p-2 lg:p-4 rounded-full shadow-sm">
+                <GrFormPrevious size={20} />
               </div>
-              <div className="button-next-custom absolute right-2 lg:right-7.5 top-1/2 -translate-y-1/2 z-10 cursor-pointer bg-white text-[#767676] p-2 lg:p-4 rounded-full shadow-md flex items-center justify-center">
-                <MdNavigateNext size={24} />
+              <div className="button-next-custom absolute right-2 lg:right-5 top-1/2 -translate-y-1/2 z-10 cursor-pointer bg-white/80 p-2 lg:p-4 rounded-full shadow-sm">
+                <MdNavigateNext size={20} />
               </div>
             </div>
           </div>
-          {/* Flex Two  */}
-          <div className="text-center lg:text-start">
-            <div className="flex  flex-col lg:flex-row lg:gap-x-71.25">
-              <h4 className="texts_14_medium text-head pt-3 lg:pt-0">
-                HOME /THE SHOP
-              </h4>
-              <div className="flex text-head texts_14_medium gap-x-7 mx-auto">
-                <button className="flex items-center gap-x-2.5 cursor-pointer">
+
+          {/* Right Side: Product Details */}
+          <div className="text-start px-4 md:px-20 lg:px-0">
+            <div className="hidden lg:flex justify-between items-center mb-10">
+              <h4 className="texts_14_medium text-head">HOME / THE SHOP</h4>
+              <div className="flex gap-x-6">
+                <button className="flex items-center gap-x-1 texts_14_medium cursor-pointer text-head">
                   <GrFormPrevious /> PREV
                 </button>
-                <button className="flex items-center gap-x-2.5 cursor-pointer">
+                <button className="flex items-center gap-x-1 texts_14_medium cursor-pointer text-head">
                   NEXT <MdNavigateNext />
                 </button>
               </div>
             </div>
-            <h3 className="lg:head_26_regular py-2 lg:pt-10 lg:pb-[7px]">
+
+            <h3 className="text-xl lg:head_26_regular font-normal text-head">
               Lightweight Puffer Jacket With a Hood
             </h3>
-            <h4 className="text-[22px] font-medium ">$449</h4>
-            <p className="texts_14_regular text-head lg:w-135 w-full leading-6 lg:pt-6.25">
+            <h4 className="text-[22px] font-medium text-head mt-2">$449</h4>
+
+            <p className="texts_14_regular text-head max-w-125 leading-6 pt-4 lg:pt-6.25">
               Phasellus sed volutpat orci. Fusce eget lore mauris vehicula
               elementum gravida nec dui. Aenean aliquam varius ipsum, non
               ultricies tellus sodales eu. Donec dignissim viverra nunc, ut
               aliquet magna posuere eget.
             </p>
-            {/* Butttons  */}
-            <div className="flex flex-col lg:flex-row mx-auto text-center justify-center lg:justify-start items-center  gap-5 my-2 lg:my-8.75">
-              <div className="w-29 h-15 border-2 border-second p-5 flex items-center justify-center gap-x-3 texts_16_regular text-[#767676]">
-                <button onClick={handleMinus} className="cursor-pointer">
+
+            {/* Buttons Container */}
+            <div className="flex items-center gap-x-2.5 lg:gap-x-5 my-6 lg:my-8">
+              {/* Quantity Selector */}
+              <div className="w-[100px] lg:w-[125px] h-12 lg:h-[60px] border border-[#e4e4e4] flex items-center justify-between px-3 lg:px-5 shrink-0">
+                <button
+                  onClick={handleMinus}
+                  className="cursor-pointer text-xl text-[#767676] hover:text-black transition-colors"
+                >
                   -
                 </button>
-                <span>{count}</span>
-                <button onClick={handlePlus} className="cursor-pointer">
+                <span className="text-head font-medium">{count}</span>
+                <button
+                  onClick={handlePlus}
+                  className="cursor-pointer text-xl text-[#767676] hover:text-black transition-colors"
+                >
                   +
                 </button>
               </div>
-              <button className="w-70 h-15 bg-head flex justify-center items-center text-white texts_14_medium  cursor-pointer">
+
+              {/* Add to Cart Button - Fixed width logic fixed here */}
+              <button className="h-12 lg:w-[280px] lg:h-[60px] bg-[#111111] text-white text-[12px] lg:text-[14px] font-medium tracking-widest cursor-pointer uppercase px-10 lg:px-0  hover:bg-black/90 transition-all active:scale-[0.98]">
                 ADD TO CART
               </button>
             </div>
-            <div className="flex justify-center lg:justify-start gap-x-7.75 py-1 mb-[32px]">
-              <h4 className="text-head text-[13px] font-medium flex gap-x-2.5 relative hover:text-head after:content-[''] after:block after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:w-[90px] after:bg-head after:transition-all after:duration-300">
-                <FaRegHeart /> ADD TO WISHLIST
-              </h4>
-              <h4 className="text-head text-[13px] font-medium flex gap-x-2.5">
-                <BsShare />
-                SHARE
-              </h4>
+
+            {/* Wishlist & Share */}
+            <div className="flex gap-x-8 lg:pb-8">
+              <button className="text-head text-[13px] font-medium flex items-center gap-x-2 uppercase relative after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-[80%] after:border-b-2 after:border-black">
+                <FaRegHeart /> Add to wishlist
+              </button>
+
+              <button className="text-head text-[13px] font-medium flex items-center gap-x-2 uppercase ">
+                <BsShare /> Share
+              </button>
             </div>
-          
-            <h5 className="texts_13_regular text-[#767676]">
-              SKU: <span className="text-head"> N/A</span>
-            </h5>
-            <h5 className="texts_13_regular text-[#767676] ">
-              Categories:{" "}
-              <span className="text-head">
-                Casual & Urban Wear, Jackets, Men
-              </span>
-            </h5>
-            <h5 className="texts_13_regular text-[#767676]">
-              Tags:{" "}
-              <span className="text-head">biker, black, bomber, leather</span>
-            </h5>
+
+            {/* Meta Info */}
+            <div className="space-y-1 mt-8">
+              <h5 className="texts_13_regular text-[#767676]">
+                SKU: <span className="text-head">N/A</span>
+              </h5>
+              <h5 className="texts_13_regular text-[#767676]">
+                Categories:{" "}
+                <span className="text-head">
+                  Casual & Urban Wear, Jackets, Men
+                </span>
+              </h5>
+              <h5 className="texts_13_regular text-[#767676]">
+                Tags:{" "}
+                <span className="text-head">biker, black, bomber, leather</span>
+              </h5>
+            </div>
           </div>
         </div>
       </Container>
