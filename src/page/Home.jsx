3cc5932206f2f34dc811fa7a5fdb6e13d64fe58b
shoumen-shomu <shoumenshomu@gmail.com>
2026-03-14 21:01:23 +0600

    merging

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,3 +1,4 @@
+import SingleBlog from "@/component/common/SingleBlog";
 import Banner from "@/component/layouts/home/Banner";
 import Collection from "@/component/layouts/home/Collection";
 import Countdown from "@/component/layouts/home/Countdown";
@@ -7,14 +8,6 @@ import Products from "@/component/layouts/home/Products";
 import Support from "@/component/layouts/home/Support";
 import Uomo from "@/component/layouts/home/Uomo";
 import React from "react";
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-import BlogList from "../component/blogPost/BlogList";
-import Blog from "@/component/common/Blog";
-import SingleBlog from "@/component/common/SingleBlog";
-=======
-
->>>>>>> 2ddc98f8a58552ba0be31d6749837760e81e010a
 
 const Home = () => {
   return (
